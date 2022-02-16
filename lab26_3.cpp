adf309005fe2ff7f083911a68b4588154deccd98
Zeronos <boss16805@gmail.com>
2022-02-16 20:58:39 +0700

    lab26_3 completed

--- lab26_3.cpp
+++ lab26_3.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include<set>
 
 using namespace std;
 
 //Write function count() here.
+int count(int *data,int size){
+    set<int> S; // declaring a set container using STL
+    for (int i = 0; i < size; i++) {
+        S.insert(data[i]); // inserting all elements of the
+                          // array into set
+    }
+    int ans = S.size(); // calculating the size of the set
+    return ans;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
